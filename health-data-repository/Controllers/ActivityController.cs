7cc9d4ce9dde1bc4178703d4afb3de2a0da1733d
Dan Monaghan <dan@daniel-monaghan.co.uk>
2018-10-31 14:02:02 +0000

    Initial project structure and example Activity model

--- health-data-repository/Controllers/ActivityController.cs
+++ health-data-repository/Controllers/ActivityController.cs
@@ -2,41 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace health_data_repository.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ActivityController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Activity
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Activity/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Activity
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Activity/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ health-data-repository/Models/Activity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace health_data_repository.Models
+{
+    /**
+     *  Class representing a single instance of 
+     *  a user's activity. For example, this model
+     *  may represent a single Workout which may be 
+     *  a jog, swim, bike ride, etc.
+    */
+    public class Activity
+    {
+
+        [Required]
+        public virtual string ActivityId { get; set; }
+
+        [Required]
+        public virtual string UserId { get; set; }
+
+        [Required]
+        public virtual DateTime DateTime { get; set; }
+
+        [Required]
+        public virtual string DataSource { get; set; }
+
+        /**
+         *  Duration of the activity in seconds 
+        */
+        public virtual int Duration { get; set; }
+
+        public virtual string ActivityType { get; set; }
+
+        public virtual int CaloriesBurnt { get; set; }
+
+        public virtual int AverageHeartRate { get; set; }
+
+        /**
+         * TODO: Needs considering, activities such as cycling 
+         * obviously won't have steps.
+        */
+        public virtual int StepsTaken { get; set; }
+
+        /**
+         *  Distance travelled during this activity,
+         *  measured in kilometers
+         *  
+         *  TODO: Check this. FitBit API says KM is default, 
+         *  but probably needs discussing
+        */
+        public virtual float DistanceTravelled { get; set; }
+
+        /**
+         * Elevation gained during this activity, measured in
+         * metres.
+         * 
+         * TODO: Check units against FitBit API responses
+        */
+        public virtual float ElevationGained { get; set; }
+    }
+}
