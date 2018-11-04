33a5300b9dd7c6f7c8f687b62c6b4e4e05db3c5d
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-04 15:56:40 +0000

    Update health-data-repository/Models/DataSource.cs
    Sticking this in an enum for simplicity for now.  Avoids needing ANOTHER table (which comes with its own UI work), but does mean new enum values would need adding for new ingestion services (trivial IMO).

---
+++ health-data-repository/Models/DataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace health_data_repository.Models
+{
+    public enum DataSource
+    {
+        Manual,
+        Fitbit
+    }
+}
