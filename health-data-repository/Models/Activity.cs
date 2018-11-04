c9dbe645c981f8464e77f1dd9c6b89896d599b81
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-04 16:04:43 +0000

    Update health-data-repository/Models/Activity.cs
    Use more descriptive field names

--- health-data-repository/Models/Activity.cs
+++ health-data-repository/Models/Activity.cs
@@ -33,27 +33,10 @@ namespace health_data_repository.Models
 
         public virtual int AverageHeartRate { get; set; }
 
-        /**
-         * TODO: Needs considering, activities such as cycling 
-         * obviously won't have steps.
-        */
         public virtual int StepsTaken { get; set; }
 
-        /**
-         *  Distance travelled during this activity,
-         *  measured in kilometers
-         *  
-         *  TODO: Check this. FitBit API says KM is default, 
-         *  but probably needs discussing
-        */
-        public virtual float DistanceTravelled { get; set; }
+        public virtual float KilometersTravelled { get; set; }
 
-        /**
-         * Elevation gained during this activity, measured in
-         * metres.
-         * 
-         * TODO: Check units against FitBit API responses
-        */
-        public virtual float ElevationGained { get; set; }
+        public virtual float MetresElevationGained { get; set; }
     }
 }
