86265872ccc6799e61742844fa91184c55e7fd81
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-21 16:14:06 +0000

    Update Startup.cs
    Add missing cookie polocy

--- HealthDataRepository/Startup.cs
+++ HealthDataRepository/Startup.cs
@@ -36,6 +36,13 @@ namespace HealthDataRepository
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<HealthDataRepositoryContext>(options =>
