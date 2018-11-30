e4030928180f71911955bfc37fc5e8f86da8080b
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-30 16:18:18 +0000

    Update ActivityRepository_Test.cs

--- HealthDataRepositoryTest/Repositories/ActivityRepository_Test.cs
+++ HealthDataRepositoryTest/Repositories/ActivityRepository_Test.cs
@@ -22,6 +22,36 @@ namespace HealthDataRepositoryTest.Repositories
         }
 
         [Fact]
+        public async void AddAsync_AddsToContext()
+        {
+            var activity = ActivityGenerator.Create();
+            using (var context = new HealthDataRepositoryContext(contextOptions))
+            {
+                context.Database.EnsureCreated();
+                var repository = new ActivityRepository(context);
+                await repository.AddAsync(activity);
+                Assert.Equal(1, await context.Activity.CountAsync());
+                Assert.Equal(activity, await context.Activity.SingleAsync());
+            }
+        }
+
+        [Fact]
+        public async void DeleteAsync_RemovesFromContext()
+        {
+            var activity = ActivityGenerator.Create();
+            using (var context = new HealthDataRepositoryContext(contextOptions))
+            {
+                context.Database.EnsureCreated();
+                context.Activity.Add(activity);
+                context.SaveChanges();
+                Assert.Equal(1, await context.Activity.CountAsync());
+                var repository = new ActivityRepository(context);
+                await repository.DeleteAsync(activity);
+                Assert.Equal(0, await context.Activity.CountAsync());
+            }
+        }
+
+        [Fact]
         public async void GetByIdAsync_ReturnsCorrectItems()
         {
             var list = ActivityGenerator.CreateList(5);
@@ -36,6 +66,67 @@ namespace HealthDataRepositoryTest.Repositories
                 var activity = await repository.GetByIdAsync(expected.Id);
                 Assert.IsType<Activity>(activity);
                 Assert.Equal(expected, activity);
+            }
+        }
+
+        [Fact]
+        public async void GetByUserIdAsync_ReturnsCorrectItems()
+        {
+            var list = ActivityGenerator.CreateList(5);
+            list[0].UserId = "someone else";
+            var expected = list.GetRange(1, 4);
+            using (var context = new HealthDataRepositoryContext(contextOptions))
+            {
+                context.Database.EnsureCreated();
+                context.Activity.AddRange(list);
+                context.SaveChanges();
+                Assert.Equal(list.Count, await context.Activity.CountAsync());
+                var repository = new ActivityRepository(context);
+                var activities = await repository.GetByUserIdAsync(expected[0].UserId);
+                Assert.IsType<List<Activity>>(activities);
+                Assert.Equal(expected, activities);
+            }
+        }
+
+        [Fact]
+        public async void GetByUserIdAsync_ReturnsCorrectItemsWithDates()
+        {
+            var list = ActivityGenerator.CreateList(5);
+            list[0].StartTimestamp = new DateTime(2020, 01, 01);
+            list[0].EndTimestamp = new DateTime(2020, 01, 02);
+            var expected = list.GetRange(1, 4);
+            using (var context = new HealthDataRepositoryContext(contextOptions))
+            {
+                context.Database.EnsureCreated();
+                context.Activity.AddRange(list);
+                context.SaveChanges();
+                Assert.Equal(list.Count, await context.Activity.CountAsync());
+                var repository = new ActivityRepository(context);
+                var activities = await repository.GetByUserIdAsync(
+                    expected[0].UserId,
+                    new DateTime(),
+                    new DateTime(2019, 12, 31)
+                );
+                Assert.IsType<List<Activity>>(activities);
+                Assert.Equal(expected, activities);
+            }
+        }
+
+        [Fact]
+        public async void UpdateAsync_UpdatesInContext()
+        {
+            var activity = ActivityGenerator.Create();
+            using (var context = new HealthDataRepositoryContext(contextOptions))
+            {
+                context.Database.EnsureCreated();
+                context.Activity.Add(activity);
+                context.SaveChanges();
+                var repository = new ActivityRepository(context);
+                var newActivity = await repository.GetByIdAsync(activity.Id);
+                newActivity.CaloriesBurnt = 5000;
+                await repository.UpdateAsync(newActivity);
+                Assert.Equal(1, await context.Activity.CountAsync());
+                Assert.Equal(newActivity, await context.Activity.SingleAsync());
             }
         }
     }
