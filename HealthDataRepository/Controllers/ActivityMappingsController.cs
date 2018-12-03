b35ed641a5f307b4936295d9279cd9488f42255e
Dan Monaghan <dan@daniel-monaghan.co.uk>
2018-12-03 21:43:17 +0000

    Remove controller actions from ActivityMappingsController which weren't needed

--- HealthDataRepository/Controllers/ActivityMappingsController.cs
+++ HealthDataRepository/Controllers/ActivityMappingsController.cs
@@ -18,36 +18,6 @@ namespace HealthDataRepository.Controllers
             _context = context;
         }
 
-        // GET: ActivityMappings
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.ActivityMapping.ToListAsync());
-        }
-
-        // GET: ActivityMappings/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var activityMapping = await _context.ActivityMapping
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (activityMapping == null)
-            {
-                return NotFound();
-            }
-
-            return View(activityMapping);
-        }
-
-        // GET: ActivityMappings/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
         // POST: ActivityMappings/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -64,91 +34,6 @@ namespace HealthDataRepository.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(activityMapping);
-        }
-
-        // GET: ActivityMappings/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var activityMapping = await _context.ActivityMapping.FindAsync(id);
-            if (activityMapping == null)
-            {
-                return NotFound();
-            }
-            return View(activityMapping);
-        }
-
-        // POST: ActivityMappings/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Source,MappingKey")] ActivityMapping activityMapping)
-        {
-            if (id != activityMapping.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(activityMapping);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ActivityMappingExists(activityMapping.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(activityMapping);
-        }
-
-        // GET: ActivityMappings/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var activityMapping = await _context.ActivityMapping
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (activityMapping == null)
-            {
-                return NotFound();
-            }
-
-            return View(activityMapping);
-        }
-
-        // POST: ActivityMappings/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var activityMapping = await _context.ActivityMapping.FindAsync(id);
-            _context.ActivityMapping.Remove(activityMapping);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ActivityMappingExists(int id)
-        {
-            return _context.ActivityMapping.Any(e => e.Id == id);
         }
     }
 }
