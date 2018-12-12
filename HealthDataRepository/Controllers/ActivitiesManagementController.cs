78bc64bdbb2c6b3c99bff5f4cc009deaa6c8965a
Adam Lancaster <arglanc@users.noreply.github.com>
2018-12-12 14:33:00 +0000

    Scaffold views

---
+++ HealthDataRepository/Controllers/ActivitiesManagementController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HealthDataRepository.Models;
+
+namespace HealthDataRepository.Controllers
+{
+    public class ActivitiesManagementController : Controller
+    {
+        private readonly HealthDataRepositoryContext _context;
+
+        public ActivitiesManagementController(HealthDataRepositoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ActivitiesManagement
+        public async Task<IActionResult> Index()
+        {
+            var healthDataRepositoryContext = _context.Activity.Include(a => a.ActivityType);
+            return View(await healthDataRepositoryContext.ToListAsync());
+        }
+
+        // GET: ActivitiesManagement/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activity
+                .Include(a => a.ActivityType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // GET: ActivitiesManagement/Create
+        public IActionResult Create()
+        {
+            ViewData["ActivityTypeId"] = new SelectList(_context.ActivityType, "Id", "Name");
+            return View();
+        }
+
+        // POST: ActivitiesManagement/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,StartTimestamp,EndTimestamp,Source,ActivityTypeId,CaloriesBurnt,AverageHeartRate,StepsTaken,MetresTravelled,MetresElevationGained")] Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(activity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ActivityTypeId"] = new SelectList(_context.ActivityType, "Id", "Name", activity.ActivityTypeId);
+            return View(activity);
+        }
+
+        // GET: ActivitiesManagement/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activity.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+            ViewData["ActivityTypeId"] = new SelectList(_context.ActivityType, "Id", "Name", activity.ActivityTypeId);
+            return View(activity);
+        }
+
+        // POST: ActivitiesManagement/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,StartTimestamp,EndTimestamp,Source,ActivityTypeId,CaloriesBurnt,AverageHeartRate,StepsTaken,MetresTravelled,MetresElevationGained")] Activity activity)
+        {
+            if (id != activity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(activity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActivityExists(activity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ActivityTypeId"] = new SelectList(_context.ActivityType, "Id", "Name", activity.ActivityTypeId);
+            return View(activity);
+        }
+
+        // GET: ActivitiesManagement/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activity
+                .Include(a => a.ActivityType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // POST: ActivitiesManagement/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var activity = await _context.Activity.FindAsync(id);
+            _context.Activity.Remove(activity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActivityExists(int id)
+        {
+            return _context.Activity.Any(e => e.Id == id);
+        }
+    }
+}
